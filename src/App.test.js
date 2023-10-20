2adab529dfe5d8d722a98cf1a90f9a84674f49ae
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-10-20 20:46:43 -0300

    Finalizado exercicio

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-// import React from 'react';
-// import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import React from 'react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import mockFetch from './__mocks__/mockFetch';
-// import renderWithRedux from './helpers/renderWithRedux';
-// import App from './App';
-// import randomNumber from './utils/randomNumber';
+import renderWithRedux from './helpers/renderWithRedux';
+import App from './App';
+import randomNumber from './utils/randomNumber';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('./utils/randomNumber');
 
 describe('Página principal', () => {
   beforeEach(() => {
@@ -14,19 +17,68 @@ describe('Página principal', () => {
     global.fetch.mockClear();
   });
 
-  test.skip('1 - Verifica se o botão de "Próximo Pokémon" está presente na tela', async () => {
-    // Remova o 'skip' e implemente seu teste
+  test('1 - Verifica se o botão de "Próximo Pokémon" está presente na tela', async () => {
+    renderWithRedux(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+
+    const nextPokemonBtn = await screen.findByRole('button');
+    expect(nextPokemonBtn).toBeInTheDocument();
   });
 
-  test.skip('2 - Verifica se foi feita uma requisição à API após carregar a página', async () => {
-    // Remova o 'skip' e implemente seu teste
+  test('2 - Verifica se foi feita uma requisição à API após carregar a página', async () => {
+    const firstPokemonId = 656;
+    const firstEndPoint = 'https://pokeapi.co/api/v2/pokemon/656/';
+
+    randomNumber.mockReturnValue(firstPokemonId);
+
+    renderWithRedux(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(firstEndPoint);
   });
 
-  test.skip('3 - Verifica se o endpoint da requisição é alterado ao clicar no botão', async () => {
-    // Remova o 'skip' e implemente seu teste
+  test('3 - Verifica se o endpoint da requisição é alterado ao clicar no botão', async () => {
+    const firstPokemonId = 656;
+    const firstEndPoint = 'https://pokeapi.co/api/v2/pokemon/656/';
+
+    const secondPokemonId = 56;
+    const secondEndPoint = 'https://pokeapi.co/api/v2/pokemon/56/';
+
+    randomNumber.mockReturnValue(secondPokemonId);
+
+    randomNumber.mockReturnValueOnce(firstPokemonId);
+
+    renderWithRedux(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(firstEndPoint);
+
+    const nextPokemonBtn = screen.getByRole('button');
+    userEvent.click(nextPokemonBtn);
+    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+    expect(global.fetch).toHaveBeenCalledWith(secondEndPoint);
   });
 
-  test.skip('4 - Verifica se os elementos contendo as informações do Pokémon são renderizados', async () => {
-    // Remova o 'skip' e implemente seu teste
+  test('4 - Verifica se os elementos contendo as informações do Pokémon são renderizados', async () => {
+    // const pokemonId = 259;
+    // const pokemonEndPoint = 'https://pokeapi.co/api/v2/pokemon/259/';
+
+    // randomNumber.mockReturnValue(pokemonId);
+
+    renderWithRedux(<App />);
+    await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
+
+    const pokemonImage = screen.getAllByAltText('pokemon');
+    expect(pokemonImage).toHaveLength(1);
+    expect(pokemonImage[0]).toBeInTheDocument();
   });
 });
